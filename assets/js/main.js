4ff1c3bbd81c4de9beb7c49d9b97be9c2c197215
GeekFamily <BrainKub@yandex.ru>
2019-02-09 23:07:19 +0300

    add Owl Carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,12 @@
 			small: '(max-width: 736px)',
 			xsmall: '(max-width: 480px)'
 		});
+		
+	$(window).on("load", function() { // makes sure the whole site is loaded
+    //preloader
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+   });
 
 	$(function() {
 
