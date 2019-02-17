ba8d43ae26919d74663fca19e46d72bef16c4654
GeekFamily <BrainKub@yandex.ru>
2019-02-17 18:55:14 +0300

    Global Update

---
+++ js/style.js
@@ -0,0 +1,27 @@
+(function($) {
+		
+	$(window).on("load", function() { // makes sure the whole site is loaded
+    //preloader
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+   });
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+	});
+
+
+
+})(jQuery);
